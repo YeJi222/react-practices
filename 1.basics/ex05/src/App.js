ecaac5b52e2a78c0aba2c73a19730ed75f1e5f77
YeJi222 <hhongyizi@gmail.com>
2023-10-31 16:05:15 +0900

    feat: babel & component

--- 1.basics/ex05/src/App.js
+++ 1.basics/ex05/src/App.js
@@ -3,9 +3,8 @@ import React from 'react';
 function App() {
     // const App = document.createElement('div');
     // App.textContent = "Hello Wolrd";
-
-    const App = React.createElement('div', null, 'Hello World');
-    return App;
+   const App = React.createElement('div', null, 'Hello World');
+   return App;
 }
 
 export {App};
