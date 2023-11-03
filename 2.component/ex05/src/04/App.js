851921be0fabe2ef77d5c8f3605a9e0806099021
YeJi222 <hhongyizi@gmail.com>
2023-11-03 15:46:03 +0900

    feat: clock assignment #finish

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -4,10 +4,18 @@ import Clock from './Clock';
 
 export default function App() {
     const [ticks, setTicks] = useState(0);
+    const [hours, setHours] = useState('00');
+    const [minutes, setMinutes] = useState('00');
+    const [seconds, setSeconds] = useState('00');
     
     useEffect(() => {
         setInterval(() => {
             /* 시간 */
+            var today = new Date();
+            setHours(today.getHours());
+            setMinutes(today.getMinutes());
+            setSeconds(today.getSeconds());
+
             setTicks(ticks + 1);
         }, 1000);
     }, [ticks]);
@@ -16,9 +24,9 @@ export default function App() {
         <>
             <Clock
                 message={`ex04: thicks ${ticks}`}
-                hours={'15'}
-                minutes={'04'}
-                seconds={'02'}/>
+                hours={hours}
+                minutes={minutes}
+                seconds={seconds}/>
         </>        
     );
 }
