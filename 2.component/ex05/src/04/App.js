0c54d1a6a047bf7d7c292dd89d4b27910feb2125
YeJi222 <hhongyizi@gmail.com>
2023-11-05 10:22:54 +0900

    feat: edit challenge assignment-#finish

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -9,15 +9,17 @@ export default function App() {
     const [seconds, setSeconds] = useState('00');
     
     useEffect(() => {
-        setInterval(() => {
+        const interval = setInterval(() => {
             /* 시간 */
-            var today = new Date();
-            setHours(today.getHours());
-            setMinutes(today.getMinutes());
-            setSeconds(today.getSeconds());
+            var clock = new Date();
+            setHours(clock.getHours().toString().length === 1 ? '0' + clock.getHours() : clock.getHours());
+            setMinutes(clock.getMinutes().toString().length === 1 ? '0' + clock.getMinutes() : clock.getMinutes());
+            setSeconds(clock.getSeconds().toString().length === 1 ? '0' + clock.getSeconds() : clock.getSeconds());
 
             setTicks(ticks + 1);
         }, 1000);
+
+        return() => clearInterval(interval); // 언마운트시 clear
     }, [ticks]);
 
     return (
